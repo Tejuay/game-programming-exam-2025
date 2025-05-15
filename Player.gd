852c9ea0d7b1136ef00a873f7cd73b4e0e2ea67a
Tejuay <thomasthejacksion1112@gmail.com>
2025-05-15 13:27:08 +0100

    PLAYER MOVES

--- Player.gd
+++ Player.gd
@@ -1,12 +1,22 @@
 extends CharacterBody2D
 
-const SPEED = 200
-@onready var is_reloading = false
-@onready var shooty_part = $ShootyPart
+var speed = 400
+var angular_speed = PI
 
-func _physics_process(delta: float) -> void:
-	look_at(get_global_mouse_position())
 
-	velocity.x = Input.get_axis("left", "right") * SPEED
-	velocity.y = Input.get_axis("up", "down") * SPEED
-	velocity = lerp(get_real_velocity(), velocity, 0.1)
+func _process(delta):
+	var direction = 0
+
+	rotation += angular_speed * direction * delta
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		velocity = Vector2.UP * speed
+	if Input.is_action_pressed("move_down"):
+		velocity = Vector2.DOWN * speed
+	if Input.is_action_pressed("move_left"):
+		velocity = Vector2.LEFT * speed
+	if Input.is_action_pressed("move_right"):
+		velocity = Vector2.RIGHT * speed
+
+	position += velocity * delta
